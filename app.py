ccdceed038bd61abe3c50e09918de7cea61fc43d
DesvoyDimitri <dimitri.desvoy@gmail.com>
2026-05-13 16:12:50 +0200

    Squelette ZERO

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Page d'accueil du site vitrine"""
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    """Page services"""
+    return render_template('services.html')
+
+@app.route('/about')
+def about():
+    """Page À propos"""
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    """Page contact"""
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
